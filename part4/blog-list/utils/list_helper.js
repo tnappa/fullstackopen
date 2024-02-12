2d385acef51649d60479beecaa3d81aaa8563777
Tuomo Nappa <tuomo.nappa@gmail.com>
2024-02-12 20:58:49 +0200

    4.4 done

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  var total = 0
+  for (const blog of blogs) {
+    total += blog.likes
+  }
+  return total
 }
 
 module.exports = {
-    dummy
+  dummy,
+  totalLikes
 }
